dc880652e478ec3eb85600fa3b210393d4c48371
andreusenis <119105848+andreusenis@users.noreply.github.com>
2022-11-28 10:51:27 +0100

    primer commit

---
+++ newmain.c
@@ -0,0 +1,53 @@
+#include <xc.h>
+#include "configuracio.h"
+#include <string.h>
+#include <stdio.h>
+#define _XTAL_FREQ 8000000
+
+void UART_init(){
+    OSCCON= 0b01110100;
+    TRISCbits.RC7 = 1;      //RX input
+    TRISCbits.RC6 = 0;      //TX output 
+    TXSTAbits.SYNC = 0;     //UART 
+    TXSTAbits.BRGH = 1;     //alta velocitat 
+    TXSTAbits.TXEN = 1;     //TX on
+    BAUDCONbits.BRG16 =1; 
+    SPBRG = 207;             //9600 a 8MHZ
+    RCSTAbits.SPEN = 1;     //UART on   
+    RCSTAbits.CREN = 1;     //RX on
+}
+
+char UART_Read()
+{
+ 
+  while(!PIR1bits.RCIF);
+  return RCREG;
+}
+void main(void) {
+    TRISAbits.RA0=0;
+    PORTA=0x00;
+    LATAbits.LATA0=0;
+    UART_init();
+    while(1)
+    {
+       char a = UART_Read();
+       while(!TXSTAbits.TRMT);
+       char b = UART_Read();
+       while(!TXSTAbits.TRMT);
+       char c = UART_Read();
+       int num = b - '0';
+       if(num >= 2)
+       {
+           LATAbits.LATA0=1;
+           __delay_ms(500);
+           LATAbits.LATA0=0;
+       }
+        if(a=='1' && b =='0' && c =='0')
+       {
+            LATAbits.LATA0=1;
+           __delay_ms(500);
+           LATAbits.LATA0=0;
+       }    
+    }
+    return;
+}
